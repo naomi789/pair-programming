51a18a4f2c718139c756c659fa49c57a3c87f1f3
naomi789 <snjohnson789@gmail.com>
2018-07-08 18:33:09 -0700

    assume sorted and no duplicates

--- magic-index.py
+++ magic-index.py
@@ -1,15 +1,46 @@
 # "A magic index in an array A[0..n-1] is defined to be an index such that A[i] = i.
 # Given a sorted array of distinct integers,
-# write a method to find a magic index, if one exists, in array A.
+# write a method to find a magic index, if one exists, in array my_array
 
-my_array = [10, 9, 8, 3]
+# 2018 July 8th
+
+my_array = [2, 3, 4, 5, 5, 5, 6]
+other_array = [1, 2, 10, 20, 30]
+some_array = [-5, -4, -3, -3, 4]
 
 
-def main(my_array):
+def simple_solution(my_array):
+    # this works even if the array is not sorted
     for value in range(0, len(my_array)):
         if my_array[value] == value:
             return value
 
 
-print(main(my_array))
+def binary_search(my_array):
+    # this works because the array is sorted
+    assert(my_array == sorted(my_array))
+    assert(len(my_array) > 0)
+
+    # and there are no duplicates
+    assert(len(my_array) == len(set(my_array)))
+
+    if my_array[0] > 0:
+        return False
+    elif my_array[len(my_array) - 1] < len(my_array) - 1:
+        return False
+    else:
+        if len(my_array) >= 2:
+            if my_array[0] < 0 and my_array[-1]:
+                pass
+
+        else:
+            pass
+            # there is only one thing
+
+
+
+
+
+# print(simple_solution(my_array))
+print(binary_search(my_array))
 
