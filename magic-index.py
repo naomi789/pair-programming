b5e44c83244b356f9143f77cbbbf6b51a0d2cd15
naomi789 <snjohnson789@gmail.com>
2018-08-26 08:41:27 -0400

    clarifying rules

--- magic-index.py
+++ magic-index.py
@@ -3,6 +3,7 @@ from math import floor
 # "A magic index in an array A[0..n-1] is defined to be an index such that A[i] = i.
 # Given a sorted array of distinct integers,
 # write a method to find a magic index, if one exists, in array my_array
+# tall numbers will be increasing, and no duplicates
 
 # 2018 July 8th
 # 2018 July 22nd 
